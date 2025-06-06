3568c871656b069f9818dbe972333eccaa7a1329
sahal9431 <sahalms9061@gmail.com>
2025-06-06 10:50:09 +0530

    new changes_2

--- src/exe_01_Java_Basic/Lab001_Helloworld.java
+++ src/exe_01_Java_Basic/Lab001_Helloworld.java
@@ -4,6 +4,10 @@ public class Lab001_Helloworld {
     public static void main(String[] args){
         System.out.println("Hello world");
         System.out.println("Hi");
-        System.out.println("Commit");
+        System.out.println(5 +2);
+        System.out.println(10-4);
+        System.out.println(10*4);
+        System.out.println(10/4);
+        System.out.println("sahal M S");
     }
 }
