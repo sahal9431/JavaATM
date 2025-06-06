8cf5709586286f6e3e3e94695aac311f1b65acd7
sahal9431 <sahalms9061@gmail.com>
2025-06-06 09:57:39 +0530

    new changes

--- src/exe_01_Java_Basic/Lab001_Helloworld.java
+++ src/exe_01_Java_Basic/Lab001_Helloworld.java
@@ -3,5 +3,6 @@ package exe_01_Java_Basic;
 public class Lab001_Helloworld {
     public static void main(String[] args){
         System.out.println("Hello world");
+        System.out.println("Hi");
     }
 }
