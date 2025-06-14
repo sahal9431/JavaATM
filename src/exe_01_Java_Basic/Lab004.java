4f00d7ff08d7cc68b1f9355dd4c7ec20e4442681
sahal9431 <sahalms9061@gmail.com>
2025-06-14 21:21:11 +0530

    changes

--- src/exe_01_Java_Basic/Lab004.java
+++ src/exe_01_Java_Basic/Lab004.java
@@ -4,6 +4,9 @@ public class Lab004 {
     public static void main(String[] args){
         array_practice();
         sum_array();
+        Reverse_array();
+        oddeven_count();
+        Second_largest_number();
     }
     public static void array_practice(){
         String[] names = {"sahal","Messi","Leo","Asif"};
@@ -19,5 +22,40 @@ public class Lab004 {
             System.out.println("sum of numbers = " + score);
         }
         System.out.println("Final sum = " + score);
+    }
+    public static void Reverse_array(){
+        int[] arr = {1,2,3,4,5};
+        for (int i = arr.length - 1; i >= 0; i--){
+            System.out.println(arr[i] + "");
+        }
+    }
+    public static void oddeven_count(){
+        int[] num = {1,2,3,4,5,6,26,29,44};
+        int even_count = 0;
+        int odd_count = 0;
+        for (int n : num){
+            if (n % 2 ==0){
+                even_count ++;
+            }else {
+                odd_count ++;
+            }
+        }
+        System.out.println("even number count = " + even_count);
+        System.out.println("Odd number count = " + odd_count);
+    }
+    public static void Second_largest_number(){
+        int[] arr ={1,2,3,4,5,66,44,5,33,45};
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        for (int n : arr){
+            if( n > first ){
+                second = first;
+                first = n;
+            }
+            else if (n > second && n != first){
+                second = n;
+            }
+        }
+        System.out.println("Second largest number = " + second);
     }
 }
